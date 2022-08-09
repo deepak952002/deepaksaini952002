b87a732fea4af856ce885de8656c69a2fcab84ca
deepak952002 <110916343+deepak952002@users.noreply.github.com>
2022-08-09 20:42:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 #load the jobfile model
 model_nb = joblib.load('spam-ham')
 
-st.title('SPAM HAM CLASSIGIER')
+st.title('SPAM HAM CLASSIFIER')
 ip = st.text_input('ENTER YOUR TEXT')
 
 op = model_nb.predict([ip])
